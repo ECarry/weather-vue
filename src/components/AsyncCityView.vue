b9923f1db4f128584150235094cff35e76f1b6b2
Administrator <admin@example.com>
2023-02-24 17:13:05 +0800

    feat: Handling Async Data

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const openweatherAPIKey = 'b726fe0ceccc42c0b27846e18951b0e1'
+
+const getWeatherData = async () => {
+  try {
+    const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${router.query.lat}&lon=${router.query.lng}&exclude=current&appid=${openweatherAPIKey}&units=metric&lang=zh_cn`
+    )
+    return weatherData
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const weatherData = await getWeatherData()
+console.log(weatherData);
+</script>
